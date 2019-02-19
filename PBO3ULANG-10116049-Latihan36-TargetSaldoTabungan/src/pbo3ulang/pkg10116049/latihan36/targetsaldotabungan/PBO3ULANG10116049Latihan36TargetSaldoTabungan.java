7b32f4c63c013cc068590897442129a703da19bf
M Ibad Guthwara <43487906+ibadguthwara@users.noreply.github.com>
2019-02-19 16:13:33 +0700

    Add files via upload

---
+++ PBO3ULANG-10116049-Latihan36-TargetSaldoTabungan/src/pbo3ulang/pkg10116049/latihan36/targetsaldotabungan/PBO3ULANG10116049Latihan36TargetSaldoTabungan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3ulang.pkg10116049.latihan36.targetsaldotabungan;
+
+/**
+ *
+ * @author ibadguthwara
+ * NAMA              : M. Ibad Guthwara
+ * KELAS             : PBO 3 ULANG
+ * NIM               : 10116049
+ * DESKRIPSI PROGRAM : Program ini berisi untuk menampilkan Target Saldo Tabungan
+ */
+public class PBO3ULANG10116049Latihan36TargetSaldoTabungan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Tabungan tabungan = new Tabungan();
+        tabungan.printTargetSaldo();
+    }
+    
+}
